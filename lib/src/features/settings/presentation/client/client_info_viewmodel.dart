b43e85ce7852b829db680bcf072b476014035f70
Theo <theolm.mota@gmail.com>
2023-04-30 14:00:09 -0300

    creating client info model

--- lib/src/features/settings/presentation/client/client_info_viewmodel.dart
+++ lib/src/features/settings/presentation/client/client_info_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:invoice_app/src/features/settings/domain/models/client_info.dart';
 import 'package:mobx/mobx.dart';
 
 part 'client_info_viewmodel.g.dart';
@@ -13,4 +14,11 @@ class ClientInfoViewModel extends _ClientInfoViewModelBase
 abstract class _ClientInfoViewModelBase with Store {
   var nameController = TextEditingController();
   var addressController = TextEditingController();
+
+  Future<void> saveInfo() async {
+    var clientInfo = ClientInfo(
+      nameController.text,
+      addressController.text,
+    );
+  }
 }
