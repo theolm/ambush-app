c2a5b763acace8327feb1e7948925da94ba94f28
Theo <theolm.mota@gmail.com>
2023-05-03 21:41:30 -0300

    Saving invoice. Still needs error treatment

--- lib/src/features/add_invoice/presentation/add_invoice_page.dart
+++ lib/src/features/add_invoice/presentation/add_invoice_page.dart
@@ -15,7 +15,10 @@ class AddInvoicePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("New invoice")),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () async {
+          var res = await _viewModel.saveInvoice();
+          print(res.toString());
+        },
         label: const Text("Generate Invoice"),
       ),
       body: ListView(
@@ -28,7 +31,25 @@ class AddInvoicePage extends StatelessWidget {
             controller: _viewModel.idController,
           ),
           const SizedBox(height: 16),
-
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: "Issue date",
+              hintText: "dd/mm/yyyy",
+            ),
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.datetime,
+            controller: _viewModel.issueDateController,
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: "Due date",
+              hintText: "dd/mm/yyyy",
+            ),
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.datetime,
+            controller: _viewModel.dueDateController,
+          ),
         ],
       ),
     );
--- lib/src/features/add_invoice/presentation/add_invoice_viewmodel.dart
+++ lib/src/features/add_invoice/presentation/add_invoice_viewmodel.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:invoice_app/src/core/data/models/hive_invoice.dart';
+import 'package:invoice_app/src/core/domain/usecases/get_bank_info.dart';
+import 'package:invoice_app/src/core/domain/usecases/get_client_info.dart';
+import 'package:invoice_app/src/core/domain/usecases/get_company_address.dart';
+import 'package:invoice_app/src/core/domain/usecases/get_company_name.dart';
+import 'package:invoice_app/src/core/domain/usecases/get_service_info.dart';
+import 'package:invoice_app/src/core/domain/usecases/save_invoice.dart';
 import 'package:mobx/mobx.dart';
 
 part 'add_invoice_viewmodel.g.dart';
@@ -7,18 +14,78 @@ part 'add_invoice_viewmodel.g.dart';
 @injectable
 class AddInvoiceViewModel extends _AddInvoiceViewModelBase
     with _$AddInvoiceViewModel {
-  AddInvoiceViewModel();
+  AddInvoiceViewModel(
+    super.getBankInfo,
+    super.getClientInfo,
+    super.getCompanyAddress,
+    super.getCompanyName,
+    super.getServiceInfo,
+    super._saveInvoice,
+  );
 }
 
 abstract class _AddInvoiceViewModelBase with Store {
+  final IGetBankInfo _getBankInfo;
+  final IGetClientInfo _getClientInfo;
+  final IGetCompanyAddress _getCompanyAddress;
+  final IGetCompanyName _getCompanyName;
+  final IGetServiceInfo _getServiceInfo;
+  final ISaveInvoice _saveInvoice;
+
+  _AddInvoiceViewModelBase(
+    this._getBankInfo,
+    this._getClientInfo,
+    this._getCompanyAddress,
+    this._getCompanyName,
+    this._getServiceInfo,
+    this._saveInvoice,
+  );
+
   var idController = TextEditingController();
   var issueDateController = TextEditingController();
   var dueDateController = TextEditingController();
-  var serviceController = TextEditingController();
-  var priceController = TextEditingController();
-  var quantityController = TextEditingController();
 
-  Future generateInvoice() async {
+  Future<bool> saveInvoice() async {
+    var bankInfo = _getBankInfo.get();
+    var clientInfo = _getClientInfo.get();
+    var companyAddress = _getCompanyAddress.get();
+    var companyName = _getCompanyName.get();
+    var serviceInfo = _getServiceInfo.get();
 
+
+    if (bankInfo == null ||
+        clientInfo == null ||
+        companyAddress == null ||
+        companyName == null ||
+        serviceInfo == null) {
+      //TODO: missing info treatment
+      return false;
+    }
+
+    var invoice = HiveInvoice(
+      int.parse(idController.text),
+      issueDateController.text,
+      dueDateController.text,
+      serviceInfo.description,
+      serviceInfo.quantity,
+      serviceInfo.currency,
+      serviceInfo.price,
+      companyName,
+      companyAddress,
+      clientInfo.name,
+      clientInfo.address,
+      bankInfo.beneficiaryName,
+      bankInfo.iban,
+      bankInfo.swift,
+      bankInfo.bankName,
+      bankInfo.bankAddress,
+      bankInfo.intermediaryBankSwift,
+      bankInfo.intermediaryBankName,
+      bankInfo.intermediaryBankAddress,
+      bankInfo.intermediaryAccNumber,
+    );
+
+    await _saveInvoice.save(invoice);
+    return true;
   }
 }
