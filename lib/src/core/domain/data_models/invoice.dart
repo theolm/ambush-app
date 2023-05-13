e61d8b43438707605ce3d0d47f2ab2a8fae383db
Theo <theolm.mota@gmail.com>
2023-05-13 19:03:39 -0300

    Creating invoice data model

---
+++ lib/src/core/domain/data_models/invoice.dart
@@ -0,0 +1,24 @@
+import 'package:invoice_app/src/core/domain/data_models/bank_info.dart';
+import 'package:invoice_app/src/core/domain/data_models/client_info.dart';
+import 'package:invoice_app/src/core/domain/data_models/comp_info.dart';
+import 'package:invoice_app/src/core/domain/data_models/service_info.dart';
+
+class Invoice {
+  final int id;
+  final int issueDate;
+  final int dueDate;
+  final ServiceInfo service;
+  final CompanyInfo companyInfo;
+  final ClientInfo clientInfo;
+  final BankInfo bankInfo;
+
+  Invoice(
+    this.id,
+    this.issueDate,
+    this.dueDate,
+    this.service,
+    this.companyInfo,
+    this.clientInfo,
+    this.bankInfo,
+  );
+}
