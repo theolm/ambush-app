5b0ddf6b7f67325cd91f10eac7ad757d24624964
Theo <theolm.mota@gmail.com>
2023-05-24 20:29:53 -0300

    Creating error dialog

--- lib/src/features/invoice_list/presentation/list_page.dart
+++ lib/src/features/invoice_list/presentation/list_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:invoice_app/src/core/di/di.dart';
+import 'package:invoice_app/src/core/presenter/components/dialog_one_button.dart';
 import 'package:invoice_app/src/core/presenter/routes/app_route.gr.dart';
 
 import 'list_page_viewmodel.dart';
@@ -31,28 +32,34 @@ class InvoiceListPage extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ),
-      body: Observer(
-          builder: (context) {
-            return ListView.separated(
-              itemCount: _viewModel.invoiceList.length,
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-              separatorBuilder: (_, __) =>
-                  Container(
-                    height: 1,
-                    width: double.infinity,
-                    color: Colors.black,
-                  ),
-              itemBuilder: (context, index) {
-                var invoice = _viewModel.invoiceList[index];
-                return Container(
-                  height: 50,
-                  alignment: Alignment.centerLeft,
-                  child: Text("Invoice Id: ${invoice.id}"),
-                );
-              },
+      body: Observer(builder: (context) {
+        return ListView.separated(
+          itemCount: _viewModel.invoiceList.length,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+          separatorBuilder: (_, __) => Container(
+            height: 1,
+            width: double.infinity,
+            color: Colors.black,
+          ),
+          itemBuilder: (context, index) {
+            var invoice = _viewModel.invoiceList[index];
+            return Container(
+              height: 50,
+              alignment: Alignment.centerLeft,
+              child: Text("Invoice Id: ${invoice.id}"),
             );
-          }
-      ),
+          },
+        );
+      }),
     );
   }
+
+  void showErrorDialog(BuildContext context) => showDialog<void>(
+        context: context,
+        builder: (BuildContext context) => DialogOneButton(
+          title: 'Title',
+          supportText: 'Testing dialog',
+          button: DialogButton('Button', () {}),
+        ),
+      );
 }
