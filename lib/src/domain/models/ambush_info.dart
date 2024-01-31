f00ed327a445233e7beb791e102b983f0d6d2b96
Emil Bellmann <emil.bellmann@getambush.com>
2024-01-31 13:46:56 -0300

    Fixed new company name

--- lib/src/domain/models/ambush_info.dart
+++ lib/src/domain/models/ambush_info.dart
@@ -1,6 +1,6 @@
 import 'package:ambush_app/src/domain/models/currency.dart';
 
-const String ambushName = "Ambush International Limited";
+const String ambushName = "Ambush Consulting International Limited";
 const String ambushAddress = "20 Harcourt St, Saint Kevin's, Dublin 2, D02 PF99, Ireland";
 
 final Currency defaultCurrency = Currency("United States dollar", "USD", "US\$");
